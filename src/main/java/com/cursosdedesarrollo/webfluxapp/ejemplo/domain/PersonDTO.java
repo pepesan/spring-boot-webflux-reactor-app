693fe937d6377bb830ddbc04bdcb9c3b6317102b
pepesan <pepesan@gmail.com>
2024-10-02 15:26:32 +0200

    pequeña modificación

--- src/main/java/com/cursosdedesarrollo/webfluxapp/ejemplo/domain/PersonDTO.java
+++ src/main/java/com/cursosdedesarrollo/webfluxapp/ejemplo/domain/PersonDTO.java
@@ -5,8 +5,6 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 
 @Data
